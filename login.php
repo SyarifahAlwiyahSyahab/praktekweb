647f7493e18ae6103cbac4746f12b053f99e1c3c
SyarifahAlwiyahSyahab <167879586+SyarifahAlwiyahSyahab@users.noreply.github.com>
2024-04-25 16:22:40 +0700

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start();
 $email = $_POST['email'];
 $password = $_POST['password'];
-if($email == "idris@usm.ac.id" && $password == "123"){
+if($email == "idris@usm.ac.id" && $password == "123" && $umur == "age >=16 && age <=44"){
     // echo "<b>Login Berhasil</b>";
     $_SESSION['login'] = true;
     header('Location: http://localhost/praktekweb/dashboard.php');
